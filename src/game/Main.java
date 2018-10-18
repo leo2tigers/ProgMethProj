8928deceb0f459f2841094eda3f4d2c21326bb03
Nut Idl <13thfloorguy@gmail.com>
2018-10-18 18:19:45 +0700

    add Main.java

---
+++ src/game/Main.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.*;
+import gameComponent.Map;
+
+public class Main {
+	ArrayList<Map> map;
+	public void setup() {
+		
+	}
+	
+	public void loop() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
